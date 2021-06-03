79edf91c9662aa07d26b50a815bd542e1e803995
agilatakishiyev <agil.atakishiyev@pashabank.az>
2021-06-03 18:05:56 +0400

    added files

--- assets/js/drag.js
+++ assets/js/drag.js
@@ -40,23 +40,53 @@
 
 const colorBoxes = Array.from(document.querySelectorAll('.color-box'));
 
-function colorMaker(element) {
+function colorMaker(element, shouldChange = false) {
   const redTone = element.getAttribute('data-red');
   const greenTone = element.getAttribute('data-green');
   const blueTone = element.getAttribute('data-blue');
-  const finalRGB = `rgb(${redTone}, ${greenTone}, ${blueTone})`;
-  element.style.backgroundColor = finalRGB;
-  return finalRGB;
+
+  if (shouldChange) {
+    element.style.backgroundColor = `rgb(${redTone}, ${greenTone}, ${blueTone})`;
+  }
+
+  return {
+    redTone,
+    greenTone,
+    blueTone
+  };
 }
 
-colorBoxes.forEach((colorBox) => {
-  const color = colorMaker(colorBox);
+colorBoxes.forEach(colorBox => {
+  const rgbObject = colorMaker(colorBox, true);
 
   colorBox.addEventListener('dragstart', (e) => {
-    e.dataTransfer.setData('text', color);
+    e.dataTransfer.setData('text', JSON.stringify(rgbObject));
   });
 });
 
 const colorCircle = document.querySelector('.color-circle');
-colorMaker(colorCircle);
+colorMaker(colorCircle, true);
+
+['dragenter', 'dragover', 'drop'].forEach(event => {
+  colorCircle.addEventListener(event, (e) => {
+    e.preventDefault();
+  });
+});
+
+colorCircle.addEventListener('drop', (e) => {
+  const stringifiedRGBObject = e.dataTransfer.getData('text');
+  const { redTone: fromRedTone, greenTone: fromGreenTone, blueTone: fromBlueTone } = JSON.parse(stringifiedRGBObject); // {redTone: "120", greenTone: "100", blueTone: "200"}
+  const { redTone, greenTone, blueTone } = colorMaker(colorCircle);  // rgb(200,200, 200);
+
+  const finalRed = Math.round((+fromRedTone + +redTone) / 2);
+  const finalGreen = Math.round((Number(fromGreenTone) + Number(greenTone)) / 2);
+  const finalBlue = Math.round((parseInt(fromBlueTone) + parseInt(blueTone)) / 2);
+
+  colorCircle.setAttribute('data-red', finalRed);
+  colorCircle.setAttribute('data-green', finalGreen);
+  colorCircle.setAttribute('data-blue', finalBlue);
+
+  const finalRGBString = `rgb(${finalRed}, ${finalGreen}, ${finalBlue})`;
+  colorCircle.style.backgroundColor = finalRGBString;
+});
 
